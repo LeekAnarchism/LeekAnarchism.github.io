4d08c9b64c6f95d37afac398790378041f15a96a
Michael Kearns <mike.john.kearns@gmail.com>
2018-07-15 18:43:49 +0100

    use rear-facing camera

--- video.js
+++ video.js
@@ -6,7 +6,7 @@
   // Put variables in global scope to make them available to the browser console.
   var constraints = window.constraints = {
     audio: false,
-    video: true
+    video: { facingMode: { exact: "environment" } }
   };
 
   function handleSuccess(stream) {
